49e1028b9e9fd18c1f146d5ab85519c053d13f82
mahimanayi19 <mahimanayi1998@gmail.com>
2025-08-12 20:29:23 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "expense-splitter-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",  // your main JS file
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
